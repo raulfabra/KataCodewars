ae2595c08a9d42be65425e3f612f280631de8b00
Raúl Fabra <fabraraul@yahoo.com>
2024-09-04 17:31:32 +0200

    Done 'who ate the cookie ?' Kata. Level 8

---
+++ Level_8/Sprint_2/who_ate_the_cookie/app.js
@@ -0,0 +1,5 @@
+function cookie(x) {
+  if (typeof x === "string") return "Who ate the last cookie? It was Zach!";
+  if (typeof x === "number") return "Who ate the last cookie? It was Monica!";
+  else return "Who ate the last cookie? It was the dog!";
+}
